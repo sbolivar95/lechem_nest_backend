e5341933812a40fa8b6bce2e76b77945643c54d3
Sebastian <sebastianbolivar95@gmail.com>
2025-12-10 18:30:05 -0400

    Cors permission fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: ['http://localhost:3000', 'https://shefa-lechem.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
