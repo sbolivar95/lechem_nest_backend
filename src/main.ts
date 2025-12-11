b2b46618de4df11500f830729f9b1189eb152eec
Sebastian <sebastianbolivar95@gmail.com>
2025-12-11 02:01:56 -0400

    cors allowed all sources

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://shefa-lechem.vercel.app',
-      'https://preview-reactubfrontend-kzmictxwwindsaz75mq6.vusercontent.net',
-    ],
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
